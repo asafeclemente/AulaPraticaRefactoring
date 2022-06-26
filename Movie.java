b0ed1c2700f15fe2fe795d00bf39b53565958dd4
Asafe <54844202+asafeclemente@users.noreply.github.com>
2022-06-26 01:20:18 +0000

    Commit 12

--- Movie.java
+++ Movie.java
@@ -6,44 +6,40 @@ public class Movie {
 
   private String _title;
   private int frequentRenterPoints = 0;
-  private int _priceCode;
+  private Price _price;
 
   public Movie(String title, int priceCode) {
       _title = title;
-      _priceCode = priceCode;
+      setPriceCode(priceCode);
   }
 
   public int getPriceCode() {
-      return _priceCode;
-  }
-
-  public void setPriceCode(int arg) {
-      _priceCode = arg;
+      return _price.getPriceCode();
   }
 
   public String getTitle (){
       return _title;
   }
 
-  public double getCharge(int daysRented){
-    //determine amounts for each line
-        double thisAmount = 0;
-        switch (this.getPriceCode()) {
-            case Movie.REGULAR:
-              thisAmount += 2;
-              if (daysRented > 2)
-                  thisAmount += (daysRented - 2) * 1.5;
-              break;
-            case Movie.NEW_RELEASE:
-              thisAmount += daysRented * 3;
-              break;
-            case Movie.CHILDRENS:
-              thisAmount += 1.5;
-              if (daysRented > 3)
-                  thisAmount += (daysRented - 3) * 1.5;
-                break;
+  public void setPriceCode(int arg) {
+      switch (arg) {
+         case REGULAR:
+            _price = new RegularPrice();
+            break;
+         case CHILDRENS:
+            _price = new ChildrensPrice();
+            break;
+         case NEW_RELEASE:
+            _price = new NewReleasePrice();
+            break;
+         default:
+            throw new IllegalArgumentException("Incorrect Price Code");
       }
-    return thisAmount;
+   }
+
+  public double getCharge(int daysRented){
+    return _price.getCharge(daysRented);
+
   }
   public int getFrequentRenterPoints(int daysRented){
         // add bonus for a two day new release rental
